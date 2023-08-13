2a1fb09a975e4a369b3ecd502936996efceb995a
wenwutang <twwinde@gmail.com>
2023-08-13 20:50:52 +0200

    update

--- datasets/dataloaders.py
+++ datasets/dataloaders.py
@@ -12,11 +12,10 @@ def get_dataloaders(opt):
 
     file = __import__("datasets."+dataset_name)
     dataset_train = file.__dict__[dataset_name].__dict__[dataset_name](opt, for_metrics=False)
-    dataset_supervised = file.__dict__[dataset_name].__dict__[dataset_name](opt,for_metrics = False ,for_supervision = True)
     dataset_val   = file.__dict__[dataset_name].__dict__[dataset_name](opt, for_metrics=True)
     print("Created %s, size train: %d, size val: %d" % (dataset_name, len(dataset_train), len(dataset_val)))
 
     dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size = opt.batch_size, shuffle = True, drop_last=True)
     dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size = opt.batch_size, shuffle = False, drop_last=False)
-    dataloader_supervised = torch.utils.data.DataLoader(dataset_supervised, batch_size = opt.batch_size, shuffle = True, drop_last=True)
-    return dataloader_train,dataloader_supervised, dataloader_val
+
+    return dataloader_train, dataloader_val
